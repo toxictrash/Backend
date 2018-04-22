4a3c56ed20e84b3602487557a518e15eef9440d8
Tom <info@toxictoast.de>
2018-04-22 04:09:18 +0200

    updated users model

--- app/Models/Overwatch/UsersModel.php
+++ app/Models/Overwatch/UsersModel.php
@@ -5,6 +5,11 @@ namespace App\Models\Overwatch;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+use App\Models\Overwatch\GuidesModel;
+// use App\Models\Overwatch\NewsModel;
+use App\Models\Overwatch\PlayersModel;
+use App\Models\Overwatch\VodModel;
+
 class UsersModel extends Model
 {
     use SoftDeletes;
@@ -23,5 +28,22 @@ class UsersModel extends Model
 
     public function scopeOnlyActive($query) {
         return $query->where('active', '1');
+    }
+
+    // Guides
+    public function guides() {
+        return $this->hasMany(GuidesModel::class, 'user_id', 'id');
+    }
+    // News
+    public function news() {
+        // return $this->hasMany(UsersModel::class, 'user_id', 'id');
+    }
+    // Players Account
+    public function players() {
+        return $this->hasOne(PlayersModel::class, 'user_id', 'id');
+    }
+    // VODs
+    public function vods() {
+        return $this->hasMany(VodModel::class, 'user_id', 'id');
     }
 }
