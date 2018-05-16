89f337462fc56ac8055f84ff2d64a2371eae8292
ToxicToast <info@toxictoast.de>
2018-05-16 17:49:30 +0200

    bugfix calendar model

--- app/Http/Controllers/Mirror/CalendarController.php
+++ app/Http/Controllers/Mirror/CalendarController.php
@@ -1,13 +1,13 @@
 <?php
 namespace App\Http\Controllers\Mirror;
 
-use App\Models\Mirror\GuidesModel;
+use App\Models\Mirror\CalenderModel;
 use App\Http\Controllers\Controller;
 
 class CronjobController extends Controller {
 
 	public function getModel() {
-		$data = new GuidesModel();
+		$data = new CalenderModel();
 		return $data->loadCalendar();
 	}
 
