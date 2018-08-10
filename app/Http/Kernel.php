802402d35b259e89dba630f8682b9748d2154e3e
ToxicToast <info@toxictoast.de>
2018-08-10 15:00:39 +0200

    new cors package

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -19,7 +19,8 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\TrustProxies::class,
-        \Barryvdh\Cors\HandleCors::class,
+        // \Barryvdh\Cors\HandleCors::class,
+        \Spatie\Cors\Cors::class
     ];
 
     /**
@@ -36,13 +37,15 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \Barryvdh\Cors\HandleCors::class,
+            // \Barryvdh\Cors\HandleCors::class,
+            \Spatie\Cors\Cors::class
         ],
 
         'api' => [
             'throttle:60,1',
             'bindings',
-            \Barryvdh\Cors\HandleCors::class
+            // \Barryvdh\Cors\HandleCors::class
+            \Spatie\Cors\Cors::class
         ],
     ];
 
@@ -62,6 +65,6 @@ class Kernel extends HttpKernel
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        'cors'  => \Barryvdh\Cors\HandleCors::class
+        'cors'  => \Spatie\Cors\Cors::class
     ];
 }
